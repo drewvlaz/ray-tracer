f1948096969a81554af66e4804059639ca94dc4a
Drew Vlasnik <drewvlaz@uchicago.edu>
2023-02-07 12:56:14 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Ray Tracer from Scratch in C++
+
+Wrote with guidance from _The Ray Tracer Challenge_ by Jamis Buck.
---
+++ src/Geometry.h
@@ -0,0 +1,16 @@
+#ifndef GEOMETRY_H
+#define GEOMETRY_H
+
+typedef struct {
+    int x;
+    int y;
+    int z;
+} Point_t;
+
+typedef struct {
+    int x;
+    int y;
+    int z;
+} Vector_t;
+
+#endif
